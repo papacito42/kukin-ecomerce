a4a1c700a791b14e963aca562d4de1cb214aedbe
papacito42 <86856800+papacito42@users.noreply.github.com>
2023-01-30 20:00:28 -0600

    Update Navbar.js

--- store/src/components/Navbar.js
+++ store/src/components/Navbar.js
@@ -11,7 +11,7 @@ function NavbarComponent() {
     const handleShow = () => setShow(true);
 
     const checkout = async () => {
-    await fetch('https://cbdshop.vercel.app/checkout', {
+    await fetch('http://cbdshop.vercel.app:4000/checkout', {
         method: "POST",
         headers: {
             'Content-Type': 'application/json'
